d4365c6a2bb50d4f0a51b3f894611e46eaf3f529
Bendadi Jamal <jamal.Bendadi@aginsurance.be>
2025-09-26 15:22:50 +0200

    tests

---
+++ Console/eHealthApp.Tests/PlanBusinessTests.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Numerics;
+
+namespace eHealthApp.Tests;
+
+using Console.Models;
+using eHealthApp.Business;
+using eHealthApp.Services.Data;
+using Moq;
+
+[TestClass]
+public class PlanBusinessTests
+{
+    [TestMethod]
+    public void CreatePlan_ShouldCallSaveOnDataService()
+    {
+        var mockService = new Mock<IDataService<Plan>>();
+        var planBusiness = new PlanBusiness(mockService.Object);
+        var plan = new Plan { PlanCode = "123", PlanName = "Test Plan" };
+
+        planBusiness.CreatePlan(plan);
+
+        mockService.Verify(s => s.Save(plan), Times.Once);
+    }
+
+    [TestMethod]
+    public void DeletePlan_ShouldCallDeleteOnDataService()
+    {
+        var mockService = new Mock<IDataService<Plan>>();
+        var planBusiness = new PlanBusiness(mockService.Object);
+        var plan = new Plan { PlanCode = "123", PlanName = "Test Plan" };
+
+        var result = planBusiness.DeletePlan(plan);
+
+        mockService.Verify(s => s.Delete(plan), Times.Once);
+        Assert.IsTrue(result);
+    }
+}
